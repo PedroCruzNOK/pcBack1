c96c074018657f677550f3057622365c1a98a46b
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-12-23 10:43:48 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require ('express');
 const app = express();
-
+app.use(
+    cors({
+      origin: ["http://159.65.171.55:3000", "http://frontend:5000"],
+    })
+  );
 
 //configuracion del servidor
 app.set('port', process.env.PORT || 3000);
